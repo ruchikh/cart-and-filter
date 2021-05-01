f6bdf3e11a0035ee6cbbf822bc4a003d083b2b5e
RuchiKh <ruchi.kharwar@vshift.com>
2021-05-01 10:22:16 +0530

    removed commented part

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,11 +4,6 @@ let url = "db.json"
 export const fetchProducts = () => (dispatch) => {
 	fetch(url)
 	.then(res => res.json())
-	// .catch((err) =>
-	// 		fetch("db.json")
-	// 			.then((res) => res.json())
-	// 			.then((data) => data.products)
-	// 	)
 		.then((data) => {
 			console.log(data, 'data')
 			dispatch({ type: FETCH_PRODUCTS, payload: data });
